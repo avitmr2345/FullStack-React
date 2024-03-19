f305914f837d36f3d29885ba4e35b626ea17e4c4
Avish <avitmr2345@gmail.com>
2024-03-19 21:18:31 +0530

    New Post creation and Home page for posts created

--- Blog Project/index.js
+++ Blog Project/index.js
@@ -1,13 +1,28 @@
 // create a home page where all posts exist, create page, update and delete page
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let all_posts = [];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {all_posts});
+});
+
+app.post("/new", (req, res) => {
+    const {title, content} = req.body;
+    all_posts.push({title, content});
+    res.redirect("/");
+});
+
+app.get("/createPost.html", (req, res) => {
+    res.sendFile(__dirname + "/public/html/createPost.html")
 });
 
 app.listen(3000, () => {
