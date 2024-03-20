39ffe09b60b6700f1f6fcacfaa1e022532f4f2ce
Avish <avitmr2345@gmail.com>
2024-03-20 18:11:04 +0530

    Update and Delete Blog functionality added

--- Blog Project/index.js
+++ Blog Project/index.js
@@ -1,30 +1,66 @@
-// create a home page where all posts exist, create page, update and delete page
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+import methodOverride from "method-override";
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 
-let all_posts = [];
+let all_blogs = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {all_posts});
+  res.render("index.ejs", { all_blogs });
 });
 
-app.post("/new", (req, res) => {
-    const {title, content} = req.body;
-    all_posts.push({title, content});
-    res.redirect("/");
+//show new Blog form
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
 });
 
-app.get("/createPost.html", (req, res) => {
-    res.sendFile(__dirname + "/public/html/createPost.html")
+//create Blog
+app.post("/createBlog", (req, res) => {
+  const id = all_blogs.length + 1;
+  const { title, content } = req.body;
+  all_blogs.push({ id, title, content });
+  res.redirect("/");
+});
+
+//show a single Blog
+app.get("/showBlog/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const blog = all_blogs.find((blog) => blog.id === id);
+  res.render("show.ejs", { blog });
+});
+
+app.get("/updateBlog", (req, res) => {
+    res.render("update.ejs");
+});
+
+app.get("/blog/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const blog = all_blogs.find((blog) => blog.id === id);
+  res.render("edit.ejs", { blog });
+});
+
+// Update a blog
+app.put("/updateBlog/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, content } = req.body;
+  const index = all_blogs.findIndex((blog) => blog.id === id);
+  all_blogs[index] = { id, title, content };
+  res.redirect("/");
+});
+
+app.get("/deleteBlog", (req, res) => {
+  res.render("delete.ejs");
+});
+
+app.delete("/deleteBlog/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  all_blogs = all_blogs.filter((blog) => blog.id !== id);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
-    console.log("Server running at port 3000.");
+  console.log("Server running at port 3000.");
 });
